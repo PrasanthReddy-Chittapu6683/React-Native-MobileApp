a8ee8165ee70001ad09535b395f4c5cd7bf5de7f
PrasanthReddy-Chittapu6683 <Prasanthreddy.chittapu@gmail.com>
2020-12-07 11:52:21 +0530

    Addedd React Native

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [Name, setName] = useState('CVPKR Reddy');
+  const [Language, setLanguage] = useState({ name: 'English', level: 5 });
+  const changeMyName = () => {
+    if (Name === 'CVPKR Reddy') {
+      setName('Prasanth Reddy')
+    } else {
+      setName('CVPKR Reddy')
+    }
+    if (Language.name === 'English') {
+      setLanguage({ name: 'German', level: 2 })
+    } else {
+      setLanguage({ name: 'English', level: 5 })
+    }
+  }
+  const [age, setAge] = useState(20)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.headerTag}>
+        Binding the values using useState() Hook:
+
+      </Text>
+      <Text>
+        Hi Am {Name} !!!! How are you
+      </Text>
+      <Text>
+        Language Name: {Language.name}
+      </Text>
+      <Text>
+        Fluency: {Language.level}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update My Name" onPress={changeMyName}></Button>
+      </View>
+      <Text style={styles.headerTag}>
+        Text Input:
+      </Text>
+      <Text>Enter Age:{age}</Text>
+      <TextInput style={styles.textbox} keyboardType="number-pad" onChangeText={(val) => setAge(val)} placeholder="Enter Age" />
+      <Text>Enter Name:{Name}</Text>
+      <TextInput style={styles.textbox} multiline onChangeText={(val) => setName(val)}
+        placeholder="Enter Name" />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +56,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    padding: 20
+  },
+  headerTag: {
+    fontSize: 20,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+
+  },
+  textbox: {
+    borderWidth: 2,
+    width: 200,
+    padding: 8,
+    borderColor: 'skyblue',
+    margin: 10
+  }
 });
